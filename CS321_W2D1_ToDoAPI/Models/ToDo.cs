0cf70bc65afa160e09a8a142e78fd5e8f24c4a2c
Matt Cameron <cameron.matt@gmail.com>
2019-06-09 13:38:08 -0500

    removed model attributes

--- CS321_W2D1_ToDoAPI/Models/ToDo.cs
+++ CS321_W2D1_ToDoAPI/Models/ToDo.cs
@@ -5,11 +5,8 @@ namespace CS321_W2D2_ToDoAPI.Models
 {
     public class ToDo
     {
-        [Required]
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(80, ErrorMessage = "Description should be no more than 80 characters.")]
         public string Description { get; set; }
 
         public bool IsComplete { get; set; }
